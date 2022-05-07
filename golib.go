2abc211c691e6abe3a49839a2973fe6538a80a81
Vitali Baumtrok <vbsw@mailbox.org>
2022-05-07 18:23:32 +0200

    golib readme

---
+++ golib.go
@@ -0,0 +1,9 @@
+/*
+ *          Copyright 2022, Vitali Baumtrok.
+ * Distributed under the Boost Software License, Version 1.0.
+ *      (See accompanying file LICENSE or copy at
+ *        http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+// Package golib contains various common functions for Go.
+package golib
