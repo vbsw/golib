65fd16dade119e54cb28e5801ef3c6695e534b6a
Vitali Baumtrok <vbsw@mailbox.org>
2022-05-07 18:50:44 +0200

    formatting

--- cb/cb.go
+++ cb/cb.go
@@ -1,9 +1,7 @@
-/*
- *          Copyright 2022, Vitali Baumtrok.
- * Distributed under the Boost Software License, Version 1.0.
- *     (See accompanying file LICENSE or copy at
- *        http://www.boost.org/LICENSE_1_0.txt)
- */
+//          Copyright 2022, Vitali Baumtrok.
+// Distributed under the Boost Software License, Version 1.0.
+//      (See accompanying file LICENSE or copy at
+//        http://www.boost.org/LICENSE_1_0.txt)
 
 // Package cb maps objects to ids. This allows to manage callbacks from C to Go.
 package cb
